c9aca7f90906c7593fded83da6d122200a74ee55
LenyaPlay <lenyaplay@mail.ru>
2023-04-27 23:00:16 +0300

    Test auth view add

--- App.tsx
+++ App.tsx
@@ -5,14 +5,18 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { cloneElement, useState } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
+  Alert,
+  Button,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -25,93 +29,75 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [savePassword, setSavePassword] = useState(true)
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const onPressSavePassword = () => {
+    setSavePassword(!savePassword);
+  }
+
+  const onSignIn = async () =>  {
+    try {
+      const response = await fetch(
+        'http://jsonplaceholder.typicode.com/posts',
+      );
+      const json = await response.json();
+      Alert.alert(json[1].id+'');
+      return json.movies;
+    } catch (error) {
+      Alert.alert(error + '');
+    }
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.centerView}>
+      <Text style={styles.text}>Авторизация</Text>
+      <TextInput style={styles.input} placeholder='Логин'></TextInput>
+      <TextInput style={styles.input} placeholder='Пароль'></TextInput>
+      <View style={{ flexDirection: 'row', alignItems: 'center', width: '75%' }}>
+        <Pressable onPress={onPressSavePassword} style={[{backgroundColor: savePassword ? '#ddd' : Colors.transparent }, styles.checkBox]}></Pressable>
+        <Text onPress={onPressSavePassword} style={{fontSize: styles.checkBox.fontSize, marginLeft: 5}}>Сохранить вход</Text>
+      </View>
+      <Pressable onPress={onSignIn}>
+        <Text style={{ color: Colors.black, fontSize: 18, margin: 25, borderWidth: 1, paddingHorizontal: 10, borderRadius: 20 }}>
+          Войти
+        </Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
 
+function onPressLearnMore() {
+  Alert.prompt('ad');
+}
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  centerView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 0.9
   },
-  sectionTitle: {
+  checkBox: {
+    borderWidth: 1,
+    width: 20,
+    height: 20,
+    borderRadius: 5,
+    justifyContent: 'center',
+    fontSize: 16,
+  },
+  text: {
     fontSize: 24,
-    fontWeight: '600',
+    color: Colors.black,
+    marginBottom: 50
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 20,
+    margin: 5,
+    width: '75%',
   },
 });
 
